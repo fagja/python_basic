45c204f0ea3754c3dbd2b9989f1a32a77186bfb8
fagja <hey.shunya19@gmail.com>
2022-06-20 12:43:50 +0900

    A5

--- assignmentA.py
+++ assignmentA.py
@@ -5,3 +5,6 @@ members = ["Bob", "Tom", "Ken"]
 
 print(members[0])
 print(members[1])
+
+# 次のリストを利用して、"Name: Bob Dylan, Age: 79"と出力してください
+print("Name: " + bob_info[0] + " " + bob_info[1] + ", Age: " + str(bob_info[2]))
