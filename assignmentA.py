34bb6ff64cfa83b3ab023f44826808fbd10c4bdf
fagja <hey.shunya19@gmail.com>
2022-06-20 12:58:53 +0900

    A6

--- assignmentA.py
+++ assignmentA.py
@@ -3,8 +3,18 @@ int_numbers = [1, 2, 3, 4, 5]
 bob_info = ["Bob", "Dylan", 79]
 members = ["Bob", "Tom", "Ken"]
 
-print(members[0])
-print(members[1])
+# A4
+# print(members[0])
+# print(members[1])
 
 # 次のリストを利用して、"Name: Bob Dylan, Age: 79"と出力してください
-print("Name: " + bob_info[0] + " " + bob_info[1] + ", Age: " + str(bob_info[2]))
+# A5
+# print("Name: " + bob_info[0] + " " + bob_info[1]
+# + ", Age: " + str(bob_info[2]))
+
+odd_numbers = [1, 3, 5, 7, 9]
+
+# A6
+for i in range(10):
+    if i % 2 == 1:
+        print(i)
